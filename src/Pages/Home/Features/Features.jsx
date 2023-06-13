c07d17323f1c94d1a701f2e84af8055c8e5aaa86
Abdur-P-Rahim <bspirahim@gmail.com>
2023-06-13 23:55:58 +0600

    Features section added

--- src/Pages/Home/Features/Features.jsx
+++ src/Pages/Home/Features/Features.jsx
@@ -1,11 +1,34 @@
 
 const Features = () => {
     return (
-        <div className="max-w-screen-lg mx-auto">
-            <div>
-                <img className="w-24" src="/public/images/ICON/book.png" alt="" />
-                <p>1500 + Topic</p>
-                <small>Learn Anythings</small>
+        <div className="md:flex justify-between items-center max-w-screen-lg mx-auto mt-20">
+            <div className="text-center">
+                <img className="w-20 mx-auto" src="/images/ICON/book.png" alt="" />
+                <div className="my-3">
+                    <p className="text-2xl font-bold pb-1">1500 + Topic</p>
+                    <p>Learn Anythings</p>
+                </div>
+            </div>
+            <div className="text-center ">
+                <img className="w-24 mx-auto" src="/images/ICON/Student.png" alt="" />
+                <div className="my-3">
+                    <p className="text-2xl font-bold pb-1">1800 + Students</p>
+                    <p>Learn Anythings</p>
+                </div>
+            </div>
+            <div className=" text-center ">
+                <img className="w-24 mx-auto" src="/images/ICON/Test.png" alt="" />
+                <div className="my-3">
+                    <p className="text-2xl font-bold pb-1">9K + Test Token</p>
+                    <p>Learn Anythings</p>
+                </div>
+            </div>
+            <div className=" text-center ">
+                <img className="w-24 mx-auto" src="/public/images/ICON/courses.png" alt="" />
+                <div className="my-3">
+                    <p className="text-2xl font-bold pb-1">1000 + Courses</p>
+                    <p>Learn Anythings</p>
+                </div>
             </div>
 
         </div>
